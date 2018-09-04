67947279a659012856ab61179162e8b46ea2fe72
Pfingstberg <pfingstberg@gmx.net>
2018-09-04 17:06:39 +0200

    Create the skeleton of a new Flask application

---
+++ headlines.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_news():
+  return "no news is good news"
+
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
