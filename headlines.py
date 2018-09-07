c69637f46b668f2b1e8e6272fa6efb564679c419
Pfingstberg <pfingstberg@gmx.net>
2018-09-07 17:06:31 +0200

    Wprowadzenie atrybutu <publication>

--- headlines.py
+++ headlines.py
@@ -10,15 +10,8 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-@app.route("/bbc")
-def bbc():
-    return get_news('bbc')
-
-@app.route("/cnn")
-def cnn():
-    return get_news('cnn')
-
-def get_news(publication):
+@app.route("/<publication>")
+def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return """<html>
